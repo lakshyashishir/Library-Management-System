4b753ccff506a32b187c567d4afdcce0bb97f94d
Lakshya Shishir <checkinglakshya@gmail.com>
2023-06-20 01:03:42 +0530

    filter books

--- routes/getBook.js
+++ routes/getBook.js
@@ -4,20 +4,14 @@ const {auth}= require("../middleware");
 const router = express.Router();
 
 router.get("/", auth, (req, res) => {
-    res.render("getBook");
-});
-
-router.post("/search", auth, (req, res) => {
-    const {bookTitle} = req.body;
-
-    db.query("SELECT * FROM books WHERE title LIKE ?", [`%${bookTitle}%`], (err, results) => {
+    db.query("SELECT * FROM books", (err, results) => {
         if (err) {
         res.status(500).json({ error: "Internal server error" });
         return;
         }
-        res.render("booklist", { books: results });
-    });
- }
-);
+        res.render("getbook", { books: results });
+    }
+    );
+});
 
 module.exports = router;
